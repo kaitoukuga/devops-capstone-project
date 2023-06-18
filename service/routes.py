c224d15e75b62582b78b7ef04235fb6137489307
kaitoukuga <greaper747@gmail.com>
2023-06-17 21:46:45 -0400

    fixing code by standart python code

--- service/routes.py
+++ service/routes.py
@@ -117,7 +117,7 @@ def update_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
 
@@ -128,6 +128,7 @@ def update_account(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
